bb52b52ca59c1a443e85835e88933d1de48dd9ce
M64GitHub <m64.overdrive@gmail.com>
2025-06-26 22:03:45 +0200

    refactor

---
+++ src/movy_player.zig
@@ -0,0 +1,41 @@
+const std = @import("std");
+const movy = @import("movy");
+const movy_video = @import("movy_video");
+
+pub const PlayerState = struct {
+    paused: bool = false,
+    stop: bool = false,
+
+    pause_start_ns: i128 = 0,
+    total_paused_ns: i128 = 0,
+
+    pub fn togglePause(
+        self: *PlayerState,
+        decoder: *movy_video.VideoDecoder,
+    ) void {
+        self.paused = !self.paused;
+        if (decoder.audio) |*a| {
+            if (self.paused) {
+                // Pause audio and measure pause time
+                self.pause_start_ns = a.getAudioClock();
+                a.pauseAudioPlayback(true);
+            } else {
+                // Continue audio and update clocks for av sync
+                const pause_end_ns = a.getAudioClock();
+                self.total_paused_ns += pause_end_ns - self.pause_start_ns;
+
+                decoder.video.start_time_ns += pause_end_ns - self.pause_start_ns;
+                a.start_time_ns += pause_end_ns - self.pause_start_ns;
+
+                a.pauseAudioPlayback(false);
+            }
+        }
+    }
+
+    pub fn getEffectiveAudioClock(
+        self: *const PlayerState,
+        audio: *movy_video.AudioState,
+    ) i128 {
+        return audio.getAudioClock() - self.total_paused_ns;
+    }
+};
--- src/movycat.zig
+++ src/movycat.zig
@@ -1,35 +1,41 @@
 const std = @import("std");
 const movy = @import("movy");
 const movy_video = @import("movy_video");
+const player = @import("movy_player.zig");
 const flagz = @import("flagz");
 
 const stdout = std.io.getStdOut().writer();
 
-var target_width: usize = 140;
-var target_height: usize = 100;
+var target_width: usize = undefined;
+var target_height: usize = undefined;
+
+const SYNC_WINDOW_NS: i64 = 50_000_000;
 
 pub fn printUsage() void {
     stdout.print(
         \\Usage:
         \\
-        \\movycat -file <filename> [-width <width> -height <height>]
+        \\movycat -file <filename> 
+        \\        [-s <startframe>] 
+        \\        [-width <width> -height <height>]
         \\
     , .{}) catch {};
 }
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{
-        .verbose_log = true,
+        // .verbose_log = true,
     }){};
 
     const allocator = gpa.allocator();
     defer _ = gpa.deinit();
 
-    // Parse args
+    // -- Parse args
     const Args = struct {
         file: []const u8,
         width: ?usize,
         height: ?usize,
+        s: ?usize,
     };
 
     const args = try flagz.parse(Args, allocator);
@@ -39,7 +45,7 @@ pub fn main() !void {
         return printUsage();
     }
 
-    // set size
+    // -- Set output dimensions
 
     const term_dimensions = try movy.terminal.getSize();
     target_width = term_dimensions.width;
@@ -52,11 +58,18 @@ pub fn main() !void {
         if (height < target_height) target_height = height;
     }
 
+    var start_frame: usize = 0;
+    if (args.s) |s| {
+        start_frame = s;
+    }
+
     // -- Setup movy screen
     try movy.terminal.beginRawMode();
     defer movy.terminal.endRawMode();
-    try movy.terminal.beginAlternateScreen();
-    defer movy.terminal.endAlternateScreen();
+
+    // Looks cooler, when last video frame stays on screen, on exit
+    // try movy.terminal.beginAlternateScreen();
+    // defer movy.terminal.endAlternateScreen();
 
     var screen = try movy.Screen.init(
         allocator,
@@ -65,7 +78,7 @@ pub fn main() !void {
     );
     defer screen.deinit(allocator);
 
-    screen.setScreenMode(movy.Screen.Mode.transparent);
+    screen.setScreenMode(movy.Screen.Mode.bgcolor);
 
     // -- init render surface for output, and add to screen
     var surface = try movy.RenderSurface.init(
@@ -76,42 +89,98 @@ pub fn main() !void {
     );
     defer surface.deinit(allocator);
 
+    // maybe center on screen
     surface.x = 0;
     surface.y = 0;
 
     try screen.addRenderSurface(surface);
 
-    // -- Open movie
+    // -- Initialize the decoder
+    const decoder =
+        try movy_video.VideoDecoder.init(allocator, args.file, surface);
+    defer decoder.deinit(allocator);
 
-    const decoder = try movy_video.VideoDecoder.init(
-        allocator,
-        args.file,
-        surface,
-    );
-    defer decoder.deinit();
+    var reached_end = false;
+    var loop_ctr: usize = 0;
 
-    // Play!
-    while (true) {
-        // Try reading input every frame
+    var state = player.PlayerState{};
+
+    while (!state.stop) {
+        loop_ctr += 1;
+
+        // esc input only works with raw terminal mode
         if (try movy.input.get()) |event| {
-            switch (event) {
-                .key => |key| {
-                    if (key.type == .Escape) {
-                        break;
-                    }
-                },
-                else => {},
+            if (event == .key and event.key.type == .Escape) {
+                state.stop = true;
+            }
+            // Outta Space
+            if (event == .key and event.key.type == .Char and
+                event.key.sequence[0] == ' ')
+            {
+                state.togglePause(decoder);
             }
         }
 
-        // Perform one decoding step
-        // This plays audio or fills the RenderSurface
-        const result = try decoder.update();
-        if (result.eof) break;
-
-        if (result.video_rendered) {
-            screen.render();
-            try screen.output();
+        if (state.paused) {
+            std.time.sleep(10_000_000);
+            continue;
         }
+
+        // FIRST: Chck if a frame is ready to render (even before decoding more)
+        if (decoder.video.queue_count > 0) {
+            if (decoder.video.peekFrame()) |head| {
+                const playback_time_ns = decoder.getAudioClock();
+                const head_pts_i64 = @as(i64, @intCast(head.pts_ns));
+                const audio_i64 = @as(i64, @intCast(playback_time_ns));
+                const diff = head_pts_i64 - audio_i64;
+
+                decoder.video.pkt_ctr += 1;
+
+                if (diff <= SYNC_WINDOW_NS and diff >= -SYNC_WINDOW_NS) {
+                    if (decoder.video.popFrame()) |frame_ptr| {
+                        decoder.video.frame_ctr += 1;
+
+                        const t_before = std.time.nanoTimestamp();
+                        decoder.video.renderFrameToSurface(frame_ptr, surface);
+                        const t_after = std.time.nanoTimestamp();
+                        const render_ns = t_after - t_before;
+
+                        if (render_ns > 10_000_000) {
+                            std.debug.print("Decoding frame took {} ns\n", .{render_ns});
+                            return error.ScalingTooSlow;
+                        }
+
+                        screen.render();
+                        try screen.output();
+
+                        movy_video.VideoDecoder.freeAVFrame(frame_ptr);
+                    }
+                } else if (diff < -SYNC_WINDOW_NS) {
+                    // Video is behind – drop the frame!
+                    _ = decoder.video.popFrame();
+                } else {
+                    // Too early ->  just wait a bit
+                    std.time.sleep(500_000);
+                }
+            }
+        }
+
+        // THEN: Decode only if queue is not full
+        if (decoder.video.queue_count < movy_video.MAX_VIDEO_FRAMES) {
+            const playback_time_ns = decoder.getAudioClock();
+            switch (try decoder.processNextPacket(SYNC_WINDOW_NS, playback_time_ns)) {
+                .eof => reached_end = true,
+                else => {}, // any outcome advances state
+            }
+        }
+
+        // All frames processed
+        if (decoder.video.queue_count == 0 and reached_end) {
+            state.stop = true;
+        }
+
+        std.time.sleep(1_000); // bit of breathing space for the cpu
     }
+
+    // The End
 }
